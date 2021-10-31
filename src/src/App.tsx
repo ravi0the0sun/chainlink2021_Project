c1a60ec070b819dc398b5213104ab446e8520f87
ravi0the0sun <28498241+ravi0the0sun@users.noreply.github.com>
2021-10-31 18:46:53 +1100

    added react-app in src

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { sprinkles } from './sprinkles.css';
+import * as styles from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export const App = () => (
+  <div
+    className={sprinkles({
+      background: {
+        lightMode: 'green-500',
+        darkMode: 'gray-900',
+      },
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      placeItems: 'center',
+      padding: '6x',
+    })}
+  >
+    <div className={styles.card}>
+      <div
+        className={sprinkles({
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: {
+            mobile: '5x',
+            desktop: '6x',
+          },
+        })}
+      >
+        <h1
+          className={sprinkles({
+            fontFamily: 'body',
+            textAlign: 'center',
+            typeSize: {
+              mobile: '4x',
+              tablet: '4x',
+              desktop: '5x',
+            },
+          })}
         >
-          Learn React
-        </a>
-      </header>
+          <span role="img" aria-label="Waving hand">
+            👋
+          </span>
+          <span role="img" aria-label="vanilla-extract logo">
+            🧁
+          </span>
+          <span role="img" aria-label="Sprinkles logo">
+            🍨
+          </span>
+        </h1>
+        <h2
+          className={sprinkles({
+            fontFamily: 'body',
+            color: {
+              lightMode: 'green-700',
+              darkMode: 'green-50',
+            },
+            textAlign: 'center',
+            typeSize: {
+              mobile: '2x',
+              tablet: '3x',
+              desktop: '4x',
+            },
+          })}
+        >
+          Hello from vanilla-extract and Sprinkles
+        </h2>
+      </div>
     </div>
-  );
-}
-
-export default App;
+  </div>
+);
